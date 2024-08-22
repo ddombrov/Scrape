be88785a439a38ae9a731fa6870b982ff2c568ef
ddombrov <ddombrov@uoguelph.ca>
2024-08-22 14:39:56 -0400

    added keywords.py

---
+++ keywords.py
@@ -0,0 +1,47 @@
+conference_keywords = {
+    'conference', 'conferences', 'proceeding', 'proceedings',
+    'workshop', 'workshops', 'meeting', 'meetings',
+    'Conference', 'Conferences', 'Proceeding', 'Proceedings',
+    'Workshop', 'Workshops', 'Meeting', 'Meetings'
+}
+
+ignored_keywords = {
+    'publication date', 'publication dates', 'authors', 'author',
+    'descriptions', 'description', 'scholar articles', 'publisher',
+    'publishers', 'volume', 'volumes', 'page', 'pages', 'issue', 'issues'
+    'Publication Date', 'Publication Dates', 'Authors', 'Author',
+    'Descriptions', 'Description', 'Scholar Articles', 'Publisher',
+    'Publishers', 'Volume', 'Volumes', 'Page', 'Pages', 'Issue', 'Issues'
+}
+
+citations_keywords = {
+    'total citations', 'total citation',
+    'Total Citations', 'Total Citation'
+}
+
+preprint_keywords = {
+    'preprint', 'preprints', 'arxiv', 'biorxiv', 'medrxiv',
+    'Preprint', 'Preprints', 'Arxiv', 'Biorxiv', 'Medrxiv'
+}
+
+journal_keywords = {
+    'journal', 'journals',
+    'Journal', 'Journals'
+}
+
+book_keywords = {
+    'book', 'books',
+    'Book', 'Books'
+}
+
+book_chapter_keywords = {
+    'book chapter', 'book chapters',
+    'Book Chapter', 'Book Chapters',
+    'book page', 'book pages',
+    'Book Page', 'Book Pages'
+}
+
+patent_keywords = {
+    'patent', 'patents',
+    'Patent', 'Patents'
+}
